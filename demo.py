1601708fc3481fb8b92fb698f3dbe6f265c4bc20
Brandon High <brandonhigh@gmail.com>
2017-01-29 23:24:12 -0800

    add notes

--- demo.py
+++ demo.py
@@ -6,32 +6,40 @@ def compute_error_for_line_given_points(b, m, points):
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
+        # https://spin.atomicobject.com/wp-content/uploads/linear_regression_error1.png
         totalError += (y - (m * x + b)) ** 2
     return totalError / float(len(points))
 
-def step_gradient(b_current, m_current, points, learningRate):
+def step_gradient(b_current, m_current, points, learning_rate):
     b_gradient = 0
     m_gradient = 0
     N = float(len(points))
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
+        # direction with respect to b and m
+        # compute partial derivative of the error function
+        # that derivative tells us which direction to go on the gradient
+        # https://spin.atomicobject.com/wp-content/uploads/linear_regression_gradient1.png
         b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
         m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
-    new_b = b_current - (learningRate * b_gradient)
-    new_m = m_current - (learningRate * m_gradient)
+    new_b = b_current - (learning_rate * b_gradient)
+    new_m = m_current - (learning_rate * m_gradient)
     return [new_b, new_m]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
-    b = starting_b
-    m = starting_m
+def gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations):
+    b = initial_b
+    m = initial_m
     for i in range(num_iterations):
         b, m = step_gradient(b, m, array(points), learning_rate)
     return [b, m]
 
 def run():
     points = genfromtxt("data.csv", delimiter=",")
+    # how fast the model will converge. we don't want it to converge too quickly
+    # because it might never be able to reduce the error
     learning_rate = 0.0001
+    # initial y = mx + b (yay constants/lines)
     initial_b = 0 # initial y-intercept guess
     initial_m = 0 # initial slope guess
     num_iterations = 1000
